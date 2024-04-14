83890445b6f38658334f1748ccf6c15f29fb53bf
letanatol <letanatol@gmail.com>
2024-04-14 22:57:48 +0300

    feat: add solution task 03 getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -69,8 +69,22 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise
+        .then((value) => {
+          resolve(value);
+        })
+        .catch(() => {
+          Promise.all(promises.map((item) => item.catch(() => 'error'))).then(
+            () => {
+              reject();
+            }
+          );
+        });
+    });
+  });
 }
 
 /**
